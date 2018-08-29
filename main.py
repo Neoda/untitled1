aa5e37e4a1bcd5932ed133078eba444d64af4ca0
neoda4ka <neoda4ka@gmail.com>
2018-08-29 03:15:45 +0300

    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import cherrypy
 import config
 
 WEBHOOK_HOST = '35.204.13.201'
-WEBHOOK_PORT = 8443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
 WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
